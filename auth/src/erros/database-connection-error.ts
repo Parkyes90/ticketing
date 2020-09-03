f20461bb352a08200dd4ae08bde35dde77bb63a8
parkyes90 <parkyes90@gmail.com>
2020-09-03 17:36:53 +0900

    add custom error abstract class

--- auth/src/erros/database-connection-error.ts
+++ auth/src/erros/database-connection-error.ts
@@ -1,7 +1,17 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
   reason = "Error connecting to database";
   constructor() {
     super();
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
   }
 }
