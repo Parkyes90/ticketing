a8245b678b86d6da6c8b893cc1cb68a642982cfe
parkyes90 <parkyes90@gmail.com>
2020-09-11 16:20:50 +0900

    add signin page

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -10,11 +10,3 @@ it("respond with details about the current user", async () => {
     .expect(200);
   expect(response.body.currentUser.email).toEqual("test@test.com");
 });
-
-it("responds with null if not authenticated", async () => {
-  const response = await request(app)
-    .get("/api/users/currentuser")
-    .send()
-    .expect(401);
-  expect(response.body.currentUser).toBeUndefined();
-});
