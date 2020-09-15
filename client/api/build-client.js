75bc3673ed661d6d0a031ff4de562ff4822add0b
parkyes90 <parkyes90@gmail.com>
2020-09-15 19:50:31 +0900

    use auth mongo env

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL: "http://192.168.99.100",
+      baseURL: "http://172.17.0.2",
       headers: req.headers,
     });
   } else {
