98906c9b54bd57d07e75124e5fbd6341d0c183eb
parkyes90 <parkyes90@gmail.com>
2020-09-18 19:36:56 +0900

    add authentication app

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -5,7 +5,14 @@ it("has a route handler listening to /api/tickets for post requests", async () =
   const response = await request(app).post("/api/tickets").send({});
   expect(response.status).not.toEqual(404);
 });
-it("can ony be accessed if the user is signed in", async () => {});
+it("can ony be accessed if the user is signed in", async () => {
+  await request(app).post("/api/tickets").send({}).expect(401);
+});
+
+it("returns a status other than 401 if the user is signed in", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+  expect(response.status).not.toEqual(401);
+});
 
 it("returns an error if an invalid title is provided", async () => {});
 
