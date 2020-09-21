5073859dda136a696481a44de926540189abd415
parkyes90 <parkyes90@gmail.com>
2020-09-21 20:11:00 +0900

    add save ticket

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/tickets";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -21,18 +22,55 @@ it("returns an error if an invalid title is provided", async () => {
   await request(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
-    .send({ title: "asdad", price: -10 })
+    .send({
+      title: "",
+      price: 10,
+    })
     .expect(400);
 
   await request(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
-    .send({ price: 10 })
+    .send({
+      price: 10,
+    })
     .expect(400);
 });
 
-it("returns an error if an invalid title is provided", async () => {});
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "asldkjf",
+      price: -10,
+    })
+    .expect(400);
 
-it("returns an error if an invalid price is provided", async () => {});
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "laskdfj",
+    })
+    .expect(400);
+});
 
-it("creates a ticket with valid inputs", async () => {});
+it("creates a ticket with valid inputs", async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+  const title = "asdff";
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title,
+      price: 20,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
+});
