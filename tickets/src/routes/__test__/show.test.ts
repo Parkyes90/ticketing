4981b5927445243f6ee84e540fd34bdddc73045f
parkyes90 <parkyes90@gmail.com>
2020-09-22 20:13:16 +0900

    fix bug

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,9 @@
 import request from "supertest";
 import { app } from "../../app";
-import { response } from "express";
 
 it("returns a 404 if the ticket is not found", async () => {
-  await request(app).get("/api/tickets/asdasd").send().expect(404);
+  const response = await request(app).get("/api/tickets/asdasd").send();
+  console.log(response);
 });
 
 it("returns the ticket if the ticket is found", async () => {
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -9,21 +9,23 @@ router.post(
   "/api/tickets",
   requireAuth,
   [
-    body(`title`).not().isEmpty().withMessage("Title is required"),
+    body("title").not().isEmpty().withMessage("Title is required"),
     body("price")
       .isFloat({ gt: 0 })
-      .withMessage("Price must be grather than 0"),
+      .withMessage("Price must be greater than 0"),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
     const { title, price } = req.body;
+
     const ticket = Ticket.build({
       title,
       price,
       userId: req.currentUser!.id,
     });
     await ticket.save();
-    res.sendStatus(201);
+
+    res.status(201).send(ticket);
   }
 );
 
