e1ae69e549d837fd3afac74a05746ec004b56625
parkyes90 <parkyes90@gmail.com>
2020-09-27 14:48:22 +0900

    add update api

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import { createTicketRouter } from "./routes/new";
 import { indexTicketRouter } from "./routes";
 import { errorHandler, NotFoundError, currentUser } from "@pystickets/common";
 import { showTicketRouter } from "./routes/show";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -20,6 +21,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 app.all("*", async () => {
   throw new NotFoundError();
 });
