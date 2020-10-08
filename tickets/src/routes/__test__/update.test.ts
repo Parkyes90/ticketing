8dab8180563adbb2f1f36258ee1b2681d06acdde
parkyes90 <parkyes90@gmail.com>
2020-10-08 19:42:21 +0900

    add complete update ticket

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -63,4 +63,29 @@ it("returns a 400 if the use provided an invalid title or price", async () => {
     .expect(400);
 });
 
-it("update ", async () => {});
+it("update the ticket provided valid inputs", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "asdfg",
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "new title",
+      price: 100,
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual("new title");
+  expect(ticketResponse.body.price).toEqual(100);
+});
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -29,7 +29,11 @@ router.put(
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
-
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price,
+    });
+    await ticket.save();
     res.send(ticket);
   }
 );
