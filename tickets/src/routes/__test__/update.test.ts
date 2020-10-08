676ce0b86db646afa55f683dc13d0e7e450b3a6d
parkyes90 <parkyes90@gmail.com>
2020-10-08 19:39:41 +0900

    add update validation test

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -43,6 +43,24 @@ it("returns a 401 if the user dose not own the ticket", async () => {
     .expect(401);
 });
 
-it("returns a 400 if the provided id does not exist", async () => {});
+it("returns a 400 if the use provided an invalid title or price", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "asdfg",
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "asdasd",
+      price: -10,
+    })
+    .expect(400);
+});
 
 it("update ", async () => {});
