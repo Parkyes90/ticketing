46f3b4f13d10fdcb362610ea812115f3d95d5c6e
parkyes90 <parkyes90@gmail.com>
2020-11-30 16:50:07 +0900

    fix typo

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -1,9 +1,10 @@
 import nats, { Stan } from "node-nats-streaming";
+
 class NatsWrapper {
   private _client?: Stan;
 
   connect(clusterId: string, clientId: string, url: string) {
-    this._client = nats.connect(clientId, clientId, { url });
+    this._client = nats.connect(clusterId, clientId, { url });
 
     return new Promise((resolve, reject) => {
       this._client!.on("connect", () => {
@@ -16,4 +17,5 @@ class NatsWrapper {
     });
   }
 }
+
 export const natsWrapper = new NatsWrapper();
