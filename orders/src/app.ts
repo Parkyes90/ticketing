842bf3cb95f78d77ae1a6269c0111b4a2710ed03
parkyes90 <parkyes90@gmail.com>
2020-12-01 11:46:39 +0900

    Add order Routers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,10 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@pystickets/common";
+import { deleteOrderRouter } from "./routes/delete";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+import { newOrderRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -14,6 +18,10 @@ app.use(
   })
 );
 app.use(currentUser);
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
 app.all("*", async () => {
   throw new NotFoundError();
 });
