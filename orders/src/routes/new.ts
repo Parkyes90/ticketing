191e17362625ccc9741e929c0d53040ae31ebd75
parkyes90 <parkyes90@gmail.com>
2020-12-01 15:19:04 +0900

    Add models

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,35 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from "@pystickets/common";
+import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  async (req: Request, res: Response) => {
+    // const { ticketId } = req.body;
+    // const ticket = await Ticket.findById(ticketId);
+    // if (!ticket) {
+    //   throw new NotFoundError();
+    // }
+    // console.log("tes123t");
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
