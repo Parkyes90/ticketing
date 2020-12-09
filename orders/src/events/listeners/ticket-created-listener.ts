0aec038e6fa6429cc2f7e3c8241b145ebc93f5cf
parkyes90 <parkyes90@gmail.com>
2020-12-09 18:09:08 +0900

    Add onMessage Event

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,10 +1,19 @@
 import { Message } from "node-nats-streaming";
 import { Subjects, Listener, TicketCreatedEvent } from "@pystickets/common";
 import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
-  queueGroupName = "orders-service";
+  queueGroupName = queueGroupName;
 
-  onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { title, price } = data;
+    const ticket = Ticket.build({
+      title,
+      price,
+    });
+    await ticket.save();
+    msg.ack();
+  }
 }
