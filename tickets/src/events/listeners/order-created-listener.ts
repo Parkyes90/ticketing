69775127ae4d94f37b4802f6ec84f54cb147a09f
parkyes90 <parkyes90@gmail.com>
2020-12-28 20:23:34 +0900

    fix(tickets): Fix typing

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,13 +1,13 @@
-import { Listener, Subjects } from "@pystickets/common";
+import { Listener, OrderCreatedEvent, Subjects } from "@pystickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/tickets";
 
-export class OrderCreatedListener extends Listener<OrderCreatedListener> {
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedListener["data"], msg: Message) {
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     const ticket = await Ticket.findById(data.ticket.id);
     if (!ticket) {
       throw new Error("Ticket not found");
