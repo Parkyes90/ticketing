4d050c076e7e796deb5ff663aedafaead3aeb617
parkyes90 <parkyes90@gmail.com>
2020-12-28 20:24:00 +0900

    test(tickets): Add test setup

---
+++ tickets/src/events/listeners/__test__/order-created-listner.test.ts
@@ -0,0 +1,35 @@
+import { OrderCreatedListener } from "../order-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/tickets";
+import { OrderCreatedEvent } from "@pystickets/common";
+import { OrderStatus } from "../../../../../common/src";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 99,
+    userId: "asdfgdsf",
+  });
+  await ticket.save();
+  const data: OrderCreatedEvent["data"] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: "asdfgdsf",
+    expiresAt: "asdfgdsf",
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+  return { listener, ticket, data, msg };
+};
+
+// it("sets")
