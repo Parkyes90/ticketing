eaf342b4aa1630ff08fbf4d33a7d1bf27a839969
parkyes90 <parkyes90@gmail.com>
2020-12-29 19:22:01 +0900

    feat(tickets): Add publishing while listening

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,8 @@ import { Listener, OrderCreatedEvent, Subjects } from "@pystickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/tickets";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+import { natsWrapper } from "../../nats-wrapper";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -14,6 +16,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     }
     ticket.set({ orderId: data.id });
     await ticket.save();
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
     msg.ack();
   }
 }
