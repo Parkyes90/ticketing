6f9f8ea396232ac05574e5d20dcbb759ff989672
parkyes90 <parkyes90@gmail.com>
2020-12-30 20:20:23 +0900

    feature(orders): Modify expiration-complete-listener.ts
    return ack if status is completed

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -20,7 +20,9 @@ export class ExpirationCompleteListener extends Listener<
     if (!order) {
       throw new Error("Order not found");
     }
-
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
     order.set({
       status: OrderStatus.Cancelled,
     });
