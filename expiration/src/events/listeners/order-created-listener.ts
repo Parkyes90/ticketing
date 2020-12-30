9e978e8375ed3e765f8f6b797ffa3d18867d5bdd
parkyes90 <parkyes90@gmail.com>
2020-12-30 19:07:24 +0900

    feat(expiration): Add expiration queue publisher

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -7,6 +7,8 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("Waiting this many milliseconds to process the job:", delay);
     await expirationQueue.add({
       orderId: data.id,
     });
---
+++ expiration/src/events/publishers/expiration-complete-publisher.ts
@@ -0,0 +1,11 @@
+import {
+  Subjects,
+  Publisher,
+  ExpirationCompleteEvent,
+} from "@pystickets/common";
+
+export class ExpirationCompletePublisher extends Publisher<
+  ExpirationCompleteEvent
+> {
+  readonly subject = Subjects.ExpirationComplete;
+}
