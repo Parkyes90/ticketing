6db8628ce14ef4e2a862b92fda6be92a3a953f90
parkyes90 <parkyes90@gmail.com>
2021-01-01 16:26:12 +0900

    fix(client): Fix client baseURL

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL: "http://192.168.49.2",
+      baseURL: "http://ingress-nginx-controller.kube-system.svc.cluster.local",
       headers: req.headers,
     });
   } else {
