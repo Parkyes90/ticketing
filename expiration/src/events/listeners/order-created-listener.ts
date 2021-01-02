19ed107f5f27fc1e434c8fa5bc84df9e5fb1481c
parkyes90 <parkyes90@gmail.com>
2021-01-02 20:44:32 +0900

    fix(expiration): add delay

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -9,9 +9,12 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
     console.log("Waiting this many milliseconds to process the job:", delay);
-    await expirationQueue.add({
-      orderId: data.id,
-    });
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      { delay }
+    );
     msg.ack();
   }
 }
