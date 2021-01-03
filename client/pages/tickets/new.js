7066ef00195acc0a75e3335eff0c845014edf113
parkyes90 <parkyes90@gmail.com>
2021-01-03 20:14:08 +0900

    feat(client): Add NewTicket Page

---
+++ client/pages/tickets/new.js
@@ -0,0 +1,18 @@
+const NewTicket = () => {
+  return (
+    <div>
+      <h1>Create a Ticket</h1>
+      <form>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Price</label>
+          <input type="text" className="form-control" />
+        </div>
+      </form>
+    </div>
+  );
+};
+export default NewTicket;
