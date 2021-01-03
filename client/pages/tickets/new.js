8f636a8c75f18b8dfbc15ab2928603b78c60c0a7
parkyes90 <parkyes90@gmail.com>
2021-01-03 20:25:16 +0900

    feat(client): Add NewTicket Event Bind

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,16 +1,64 @@
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
 const NewTicket = () => {
+  const [state, setState] = useState({
+    title: "",
+    price: "",
+  });
+  const { doRequest, errors } = useRequest({
+    url: "/api/tickets",
+    method: "post",
+    body: state,
+    onSuccess: (ticket) => console.log(ticket),
+  });
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setState((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const onBlur = () => {
+    const value = parseFloat(state.price);
+
+    if (isNaN(value)) {
+      return;
+    }
+    setState({ ...state, price: value.toFixed(2) });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await doRequest();
+  };
+
   return (
     <div>
       <h1>Create a Ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            name="title"
+            onChange={onChange}
+            value={state.title}
+          />
         </div>
         <div className="form-group">
           <label>Price</label>
-          <input type="text" className="form-control" />
+          <input
+            type="number"
+            onBlur={onBlur}
+            className="form-control"
+            name="price"
+            onChange={onChange}
+            value={state.price}
+          />
         </div>
+        {errors}
+        <button className="btn btn-primary">Submit</button>
       </form>
     </div>
   );
