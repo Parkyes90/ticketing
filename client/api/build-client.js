921c1520c2f7b50a52d34984be7257d65eaa3531
YeseungPark <parkyes90@gmail.com>
2021-01-09 16:45:49 +0900

    ci(ticketing): fix bug

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL: "http://ingress-nginx-controller.kube-system.svc.cluster.local",
+      baseURL: "http://www.parkyes90-app.xyz/",
       headers: req.headers,
     });
   } else {
